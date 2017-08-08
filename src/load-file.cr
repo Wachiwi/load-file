3dc568d28164a573cfaa77bcb42109fbf27241e5
David Schwarzmann <dev@dschwarz.codes>
2017-08-08 15:33:24 +0200

    [MOD] Moved from commander to cli and refactored project

--- src/load-file.cr
+++ src/load-file.cr
@@ -1,133 +1,91 @@
 require "logger"
 require "gitlab"
-require "commander"
+require "cli"
 require "colorize"
-
-require "./load-file/*"
 
 module LoadFile
   #
-  class LoadFile
+  class LoadFile < Cli::Command
+    version "0.1.0"
+
+    @log = Logger.new(STDOUT)
+
+    class Help
+      header "load-file - Simple cli tool to load files from a customizable gitlab instance. (Intended use is for CI systems)"
+    end
+
+    class Options
+      arg "project", desc: "project", required: true
+      arg "file", desc: "file", required: true
+
+      string %w(-i --instance), desc: "Specifies the URL of the gitlab instance.", default: "https://gitlab.com"
+      bool %w(-k --insecure), desc: "Specifies if self signed certificates should be ignored. (Currently not used)", default: false
+      string %w(-t --token), desc: "Specifies the Access-Token that allows you access to the projects inside your gitlab instance.", default: nil
+      string %w(-r --ref), desc: "Specifies the Access-Token that allows you access to the projects inside your gitlab instance.", default: "master"
+      help
+    end
+
     # 2. Configuration
     #      - Check configuration (only user-based)
     #      - Parse configuration
     #      - Validate configuration
     #      - Merge with defaults
     def init_config
+      # use a ~/.lf_cfg file
       @log.debug("Loaded config")
     end
 
-    # 3. Commandline arguments
-    #      - Parse commandline args
-    #      - Merge with configuration
-    def init_cli(options, arguments)
-      options.string["instance"] += '/' unless options.string["instance"].ends_with? '/'
-      @gitlab = Gitlab.client "#{options.string["instance"]}api/v3", options.string["token"]
-      @log.debug("Initialized cli")
-    end
+    def run
+      if options.token?.nil?
+        puts "[ERROR] Token cannot be empty".colorize :red
+        Process.exit 1
+      end
 
-    # 4. Execute the query
-    def query_file
-      @log.debug("Created logger")
-    end
+      # check_project_arg args.project
+      # check_file_arg args.file
+      if args.project.empty? || args.file.empty?
+        puts "[ERROR] project and file argument needs to be set".colorize :red
+        Process.exit 1
+      end
 
-    #      4.1 Get the right repository
-    #          - Check if repository is accessible for user
-    #          - If repos with the same name are available in multiple namespaces the user gets
-    #          prompted to select one
-    #          -
-    def query_repository
-      @log.debug("Created logger")
-    end
+      #self.init_config
 
-    #      4.2 Get the file
-    #          -
-    def query_file_content
-      @log.debug("Created logger")
-    end
+      if options.instance.ends_with? '/'
+        instance = options.instance
+      else
+        instance = options.instance + '/'
+      end
 
-    # 5. Output the file contents to stdout
-    def print_file
-      @log.debug("Created logger")
-    end
+      gitlab = Gitlab.client "#{instance}api/v3", options.token
+      if gitlab.nil?
+        puts "[Error] Could not connect to instance with token!"
+        Process.exit 1
+      end
 
-    def initialize(opts, args)
-      @log = Logger.new(STDOUT)
-      @log.progname = "load-file"
-      @log.level = Logger::DEBUG
-      self.init_config
-      @log.info("Initialization finished")
-      opts.string["instance"] += '/' unless opts.string["instance"].ends_with? '/'
+      projects = gitlab.projects
+      project = nil
+
+      projects.each do |proj|
+        if proj["path_with_namespace"] == args.project
+          project = proj
+          break
+        end
+      end
+
+      if project.nil?
+        puts "[Error] Could not find project named '#{args.project}'!".colorize :red
+        Process.exit 1
+      end
 
       begin
-        @gitlab = Gitlab.client "#{opts.string["instance"]}api/v3", opts.string["token"]
-        if @gitlab.is_nil?
-          puts "[Error] Could not connect to instance with token!"
-          Process.exit 1
-        else
-          @log.debug("Initialized cli")
-          projects = @gitlab.projects
-          projects.each do |p|
-            puts p.name_with_namespace
-          end
-        end
-      rescue ex
-        pp ex.message
+        file = gitlab.get("/projects/#{project["id"]}/repository/files/#{(URI.escape args.file).gsub '.', "%2E"}/raw?ref=#{options.ref}")
+
+        pp file.body
+      rescue Gitlab::Error::NotFound
+        p "[Error] Could not find the file '#{args.file}' at ref '#{options.ref}'".colorize :red
       end
     end
   end
 end
 
-cli = Commander::Command.new do |cmd|
-  cmd.use = "load-file"
-  cmd.long = "Simple cli tool to load files from a customizable gitlab instance. (Intended use is for CI systems)"
-
-  cmd.flags.add do |flag|
-    flag.name = "instance"
-    flag.short = "-i"
-    flag.long = "--instance"
-    flag.default = "https://gitlab.com"
-    flag.description = "Specifies the URL of the gitlab instance."
-  end
-
-  cmd.flags.add do |flag|
-    flag.name = "insecure"
-    flag.short = "-k"
-    flag.long = "--insecure"
-    flag.default = false
-    flag.description = "Specifies if self signed certificates should be ignored. (Currently no usage)"
-  end
-
-  cmd.flags.add do |flag|
-    flag.name = "token"
-    flag.short = "-t"
-    flag.long = "--token"
-    flag.default = ""
-    flag.description = "Specifies the Access-Token that allows you access to the projects inside your gitlab instance."
-  end
-
-  cmd.flags.add do |flag|
-    flag.name = "ref"
-    flag.short = "-r"
-    flag.long = "--ref"
-    flag.default = "master"
-    flag.description = "Specifies the Access-Token that allows you access to the projects inside your gitlab instance."
-  end
-
-  cmd.run do |options, arguments|
-    if options.string["token"].empty?
-      puts "  [ERROR] Token cannot be empty\n".colorize :red
-      puts cmd.help
-      Process.exit 1
-    end
-
-    if arguments.size != 2
-      puts "  [ERROR] Wrong number of arguments (expected: 2, got: #{arguments.size})\n".colorize :red
-      puts cmd.help
-      Process.exit 1
-    end
-    lf = LoadFile::LoadFile.new options, arguments
-  end
-end
-
-Commander.run(cli, ARGV)
+LoadFile::LoadFile.run ARGV
